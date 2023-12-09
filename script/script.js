9fe22c3bcb468dea1838e4d7fa3a4976c4deef52
piero.carimi <pierocarimi@gmail.com>
2023-12-09 20:18:45 +0100

    feat(methods): added 'visualizzavoti' and 'inseriscivoto' methods in script.js file

--- script/script.js
+++ script/script.js
@@ -18,12 +18,9 @@ class RegistroClasse {
         }else{
             this.studenti =JSON.parse(localStorage.getItem("this.studenti"));
         }
-        
-        for(let student of this.studenti){
-            if (this.currentId === student.id){
-                this.currentId++;
-            }
-        }
+
+        const maxId = Math.max(...this.studenti.map(student => student.id), 0);
+        this.currentId = maxId + 1;
 
         var nuovoStudente = {
             name: name,
@@ -85,10 +82,11 @@ class RegistroClasse {
             html += '<td class="text-center">' + element.id + "</td>";
             html += '<td class="text-center">' + element.name + "</td>";
             html += '<td class="text-center">' + element.lastName + "</td>";
-            html += '<td class="text-center"><button onclick="visualizzaVoti()" class="btn btn-outline-dark m-2">Voti ↓</button>'
+            html += '<td class="text-center"><button onclick="register.visualizzaVoti('+element.id+')" class="btn btn-outline-dark m-2">Voti ↓</button>'
             html += 
             '<td class="text-center"><button onclick="register.modificaStudente('+element.id+')"class="btn btn-outline-secondary">Edit</button><button onclick="register.rimuoviStudente('+element.id+')"class="btn btn-outline-danger m-2">Delete</button></td>';
             html +="</tr>";
+            html += `<tr><td colspan='6'><div id='votiDetails_${element.id}' style='display: none;'> <h4 class="d-flex justify-content-center">Student grades</h4></div></td></tr>`;             
             i++;
         });
 
@@ -153,6 +151,141 @@ class RegistroClasse {
             document.getElementById("updateStudent").style.display = "none";  
         }).bind(this);
     }  
+
+    visualizzaVoti(id) {
+        const sezioneVotiId = `votiDetails_${id}`;
+
+        // Sezione dei voti corrispondente a questo studente
+        const sezioneVoti = document.getElementById(sezioneVotiId);
+
+        // Verifica lo stato attuale di visualizzazione
+        const isVisualizzato = sezioneVoti.style.display === "block";
+
+        // Nascondi tutte le sezioni dei voti tranne quella corrispondente a questo studente
+        document.querySelectorAll('[id^="votiDetails_"]').forEach((element) => {
+            element.style.display = "none";
+        });
+
+        // Se la sezione dei voti era già visualizzata, nascondila
+        if (isVisualizzato) {
+            sezioneVoti.style.display = "none";
+        } else {
+            // Altrimenti, mostra la sezione dei voti corrispondente a questo studente
+            sezioneVoti.style.display = "block";
+        }
+
+        // Rimuovi il form esistente se presente
+        const existingForm = sezioneVoti.querySelector('form');
+        if (existingForm) {
+            existingForm.remove();
+        }
+
+        const existingTable = sezioneVoti.querySelector('table');
+        if (existingTable) {
+            existingTable.remove();
+        }
+        
+        const formHtml = `
+            <form onsubmit="register.inserisciVoto(${id}, this); return false;">
+            <div class="row">
+            <div class="col">
+                <label for="voto"><b>Voto:</b></label>
+                <input type="number" class="form-control" placeholder="Voto" name="voto" id="voto" required>
+            </div>
+            <div class="col">
+                <label for="data"><b>Data:</b></label>
+                <input type="date" class="form-control" name="data" id="data" required>
+            </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col">
+                    <label for="commento"><b>Commento:</b></label>
+                    <textarea class="form-control" placeholder="Commento" name="commento" id="commento"></textarea>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center mt-4 mb-4">
+                <button type="submit" class="btn btn-success" id="addVoto" onclick="register.inserisciVoto(${id},this)">Add Voto</button>
+                <button type="submit" class="btn btn-primary" id="updateVoto" onclick="register.modificaVoto()">Update Voto</button>
+            </div>
+            </form>
+            <table class="table table-bordered" id="votiTable">
+                <thead>
+                    <tr class="align-middle">
+                        <th scope="col" class="text-center">#</th>
+                        <th scope="col" class="text-center">Data</th>
+                        <th scope="col" class="text-center">Voto</th>
+                        <th scope="col" class="text-center">Commento</th>
+                        <th scope="col" class="text-center">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${this.studenti
+                        .find((studente) => studente.id === id)
+                        .voti.map((voto, index) => `
+                            <tr class="align-middle">
+                                <th scope="row" class="text-center">${index + 1}</th>
+                                <td class="text-center">${voto.data}</td>
+                                <td class="text-center">${voto.voto}</td>
+                                <td class="text-center">${voto.commento}</td>
+                                <td class="text-center"><button onclick="register.modificaStudente('+element.id+')"class="btn btn-outline-secondary">Edit</button><button onclick="register.rimuoviStudente('+element.id+')"class="btn btn-outline-danger m-2">Delete</button></td>
+                            </tr>
+                        `).join('')
+                    }
+                </tbody>
+                </table>
+            `;
+
+        // Aggiungi il form sotto la sezione dei voti
+        sezioneVoti.innerHTML += formHtml;
+    }
+
+    inserisciVoto(id, form) {
+        if (!form) {
+            console.error("Form is undefined");
+            return;
+        }
+    
+        const votoInput = form.querySelector('#voto');
+        const dataInput = form.querySelector('#data');
+        const commentoInput = form.querySelector('#commento');
+    
+        // Verifica se gli input sono presenti prima di accedere ai loro valori
+        const voto = (votoInput && votoInput.value) ? votoInput.value.toString().trim() : '';
+        const data = (dataInput && dataInput.value) ? dataInput.value.trim() : '';
+        const commento = (commentoInput && commentoInput.value) ? commentoInput.value.trim() : '';
+    
+        console.log("Voto:", voto);
+        console.log("Data:", data);
+        console.log("Commento:", commento);
+    
+        if (voto.trim() === '' || data.trim() === '' || commento.trim() === '') {
+            alert("Inserisci tutti i dati prima di inviare il form.");
+            return;
+        }
+    
+        const studente = this.studenti.find((studente) => studente.id === id);
+    
+        if (studente) {
+            // Aggiungi il nuovo voto allo studente
+            studente.voti.push({
+                voto: voto,
+                data: data,
+                commento: commento
+            });
+    
+            // Aggiorna il localStorage con l'array aggiornato di studenti
+            localStorage.setItem("this.studenti", JSON.stringify(this.studenti));
+    
+            // Aggiorna la visualizzazione degli studenti
+            this.visualizzaStudenti();
+            votoInput.value = ""; // Pulisci i campi del form
+            dataInput.value = "";
+            commentoInput.value = "";
+        } else {
+            console.error("Studente non trovato con l'id:", id);
+        }
+    }
+
 }
 
 var register = new RegistroClasse();
